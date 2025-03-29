50064e59586356c86367c20b77cb41af7abdd770
Theophilus <theondukwe@gmail.com>
2025-03-29 11:11:43 -0400

    assignment completed

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-# 📊 Project: Complex API 2 - CAR DATA API
+# 📊 Project: Complex API 2
 
 ### Goal: Use data returned from one api to make a request to another api and display the data returned
 
-### Campare regions for arbitrage opportunity
+### User enters city and country and then gets weather information as well as country flag
 
 ```
+I feel good about my code: 5
+I completed the challenge: 5
+
+I spent too much time trying to find a more useful api. I decided to just do a complex weather API
+I really like working with API's. This was fun!
 
 ```
--- js/main.js
+++ js/main.js
@@ -1,1 +1,54 @@
 // Javascript Behavior
+
+
+document.querySelector('button').addEventListener('click', searchLocation)
+
+//function to fetch info from weather site
+function searchLocation() {
+    //store user input in variable
+    let city = document.querySelector('#city').value
+    let country = document.querySelector('#country').value
+    let Key = 'cc6c6f28e7b84a72bd62b604a822491c'
+    let url = `http://api.weatherbit.io/v2.0/current?key=${Key}&city=${city}&country=${country}`
+
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            let fahrenheit = (data.data[0].temp* 9/5) + 32
+            document.querySelector('h2').innerText = data.data[0].city_name
+
+            document.querySelector('p').innerText =
+                `It is ${fahrenheit}° in ${data.data[0].city_name},${data.data[0].country_code} `
+            fetch('https://countriesnow.space/api/v0.1/countries/flag/images', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    iso2: data.data[0].country_code
+                
+                })
+
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    document.querySelector('#img-1').src = data.data.flag
+                })
+
+    .catch(err => {
+     console.log(`error ${err}`)
+ })
+            
+            })
+            .catch(err => { 
+            console.log(`error ${err}`)
+        })
+
+    
+
+
+}
+
